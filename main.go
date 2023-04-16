3d0b6883c3e4d2ee32f4a41226a0fa73ab26350b
a.lotfi <a.lotfi@pio.ir>
2023-04-16 22:26:03 +0330

    add test

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ const (
 var (
 	board [SIZE][SIZE]int
 	count = SIZE
+	res [][SIZE][SIZE]int
 )
 
-var res [][SIZE][SIZE]int
 
 func main() {
 	PlaceQueen(0)
@@ -44,6 +44,14 @@ func IsValid(row, col int) bool {
 			return false
 		}
 	}
+
+	for i := 0; i < col; i++ {
+		if board[row][i] == 1 {
+			return false
+		}
+	}
+
+
 	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
 		if board[i][j] == 1 {
 			return false
@@ -65,4 +73,61 @@ func PrintBoard(board [SIZE][SIZE]int) {
 		fmt.Println()
 	}
 	fmt.Println()
+}
+
+
+func CheckColision() bool{
+	for i := 0; i < count; i++ {
+		for j := 0; j < count; j++ {
+			if board[i][j] == 1 {
+				if !IsValid(i, j) {
+					fmt.Println("error---------------------------------------")
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func CheckColisionWitharg(aa [SIZE][SIZE]int) bool{
+	for i := 0; i < count; i++ {
+		for j := 0; j < count; j++ {
+			if aa[i][j] == 1 {
+				if !IsValidWithargs(i, j,aa) {
+					fmt.Println("error---------------------------------------")
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func IsValidWithargs(row, col int,aa [SIZE][SIZE]int ) bool {
+	for i := 0; i < row; i++ {
+		if aa[i][col] == 1 {
+			return false
+		}
+	}
+
+
+
+	for i := 0; i < col; i++ {
+		if aa[row][i] == 1 {
+			return false
+		}
+	}
+
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if aa[i][j] == 1 {
+			return false
+		}
+	}
+	for i, j := row-1, col+1; i >= 0 && j < count; i, j = i-1, j+1 {
+		if aa[i][j] == 1 {
+			return false
+		}
+	}
+	return true
 }
